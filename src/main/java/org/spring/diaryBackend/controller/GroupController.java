fa03fcdac2b65a517dbe6732a2aebf0674bffade
IvanColj <robskovv@gmail.com>
2024-10-25 10:10:40 +0300

    1

---
+++ src/main/java/org/spring/diaryBackend/controller/GroupController.java
@@ -0,0 +1,42 @@
+package org.spring.diaryBackend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.spring.diaryBackend.model.Group;
+import org.spring.diaryBackend.service.GroupService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/groups")
+@RequiredArgsConstructor
+public class GroupController {
+    private final GroupService service;
+
+    @GetMapping
+    public List<Group> getByAllGroups(
+            @RequestParam(required = false, defaultValue = "0") int offset,
+            @RequestParam(required = false, defaultValue = "5") int limit) {
+        return service.findByAllGroup(offset, limit);
+    }
+
+    @GetMapping("all")
+    public List<Group> getAllGroups() {
+        return service.findAll();
+    }
+
+    @PostMapping("save_group")
+    public Group createGroup(@RequestBody Group group) {
+        return service.saveGroup(group);
+    }
+
+    @PutMapping("update_group")
+    public Group updateGroup(@RequestBody Group group) {
+        return service.updateGroup(group);
+    }
+
+    @DeleteMapping("delete_group/{group}")
+    public void deleteNumberGroup(@PathVariable Long group) {
+        service.deleteNumberGroup(group);
+    }
+}
