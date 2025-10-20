a7f7177b235e623b7975b1bc454df08f9a570489
Иван Робсков <robskovv@gmail.com>
2025-10-21 00:27:58 +0300

    1

--- src/main/java/org/spring/diaryBackend/controller/PathController.java
+++ src/main/java/org/spring/diaryBackend/controller/PathController.java
@@ -2,6 +2,7 @@ package org.spring.diaryBackend.controller;
 
 import lombok.AllArgsConstructor;
 import org.spring.diaryBackend.dto.entity.PathDTO;
+import org.spring.diaryBackend.model.Path;
 import org.spring.diaryBackend.repository.PathRepository;
 import org.spring.diaryBackend.service.PathService;
 import org.springframework.http.HttpHeaders;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
@@ -36,14 +36,14 @@ public class PathController {
     @GetMapping("/id/{id}")
     public ResponseEntity<byte[]> downloadFile(@PathVariable Long id) {
         byte[] fileContent = pathService.downloadFile(id);
+        Path pathGet = pathRepository.findById(id).orElseThrow();
 
-        String encodedFileName = URLEncoder.encode(pathRepository.findById(id).orElseThrow().getNameFile(), StandardCharsets.UTF_8)
-                .replace("+", "%20");
+        String contentDisposition = "attachment; filename=\"" +
+                new String(pathGet.getNameFile().getBytes(StandardCharsets.UTF_8), StandardCharsets.ISO_8859_1) + "\"";
 
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .header(HttpHeaders.CONTENT_DISPOSITION,
-                        "attachment; filename*=UTF-8''" + encodedFileName)
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
                 .body(fileContent);
     }
 
